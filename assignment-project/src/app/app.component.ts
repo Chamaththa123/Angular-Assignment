2801dfb0761e696ead3245be4afba17a516e3c3a
Chamaththa123 <shamodchamaththa@gmail.com>
2025-05-22 00:00:53 +0530

    complete basic structure of the project

--- assignment-project/src/app/app.component.ts
+++ assignment-project/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'assignment-project';
-}
+export class AppComponent {}
