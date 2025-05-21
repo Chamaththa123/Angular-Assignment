0f102b613d81994e1e0a9894a8e30f4b68baee51
Chamaththa123 <shamodchamaththa@gmail.com>
2025-05-21 23:35:21 +0530

    add book list component

--- assignment-project/src/app/app.routes.ts
+++ assignment-project/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { NgModule } from '@angular/core';
+import { BookListComponent } from './pages/books/book-list/book-list.component';
 
 export const routes: Routes = [
      {
     path: '',
     component: MainLayoutComponent,
     children: [
-    //   { path: '', component: HomeComponent },
+      { path: '', component: BookListComponent },
     ]
   }
 ];
